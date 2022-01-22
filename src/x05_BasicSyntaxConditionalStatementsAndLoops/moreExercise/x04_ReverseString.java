be7d04ac030baae654c1d5e258378d0c5f1ba287
I-Zafirov <ivoomail@gmail.com>
2022-01-22 14:50:18 +0200

    commit

---
+++ src/x05_BasicSyntaxConditionalStatementsAndLoops/moreExercise/x04_ReverseString.java
@@ -0,0 +1,17 @@
+package x05_BasicSyntaxConditionalStatementsAndLoops.moreExercise;
+
+import java.util.Scanner;
+
+public class x04_ReverseString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        String reverse = "";
+
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reverse += input.charAt(i);
+        }
+        System.out.println(reverse);
+    }
+}
