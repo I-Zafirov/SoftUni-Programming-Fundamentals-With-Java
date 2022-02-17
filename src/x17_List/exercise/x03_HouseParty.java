e3a36dd0acdf2c9badd0eed4b46c20e2e19f3bdf
I-Zafirov <ivoomail@gmail.com>
2022-02-17 11:01:51 +0200

    commit

---
+++ src/x17_List/exercise/x03_HouseParty.java
@@ -0,0 +1,35 @@
+package x17_List.exercise;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class x03_HouseParty {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<String> guests = new ArrayList<>();
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < n; i++) {
+            String line = scanner.nextLine();
+            String[] data = line.split("\\s+");
+            String name = data[0];
+            if (!line.contains("is not going!")) { // if (data.length == 3){
+                if (guests.contains(name)) {
+                    System.out.printf("%s is already in the list!%n", name);
+                } else {
+                    guests.add(name);
+                }
+            } else {
+                if (guests.contains(name)) {
+                    guests.remove(name);
+                } else {
+                    System.out.printf("%s is not in the list!%n", name);
+                }
+            }
+        }
+        //guests.forEach(guests -> System.out.println(guest));
+        guests.forEach(System.out::println);
+    }
+}
---
+++ src/x17_List/exercise/x07_AppendArrays2.java
@@ -0,0 +1,25 @@
+package x17_List.exercise;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class x07_AppendArrays2 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        List<String> numbers= Arrays
+                .stream((scanner.nextLine().split("\\|")))
+                .collect(Collectors.toList());
+        Collections.reverse(numbers);
+
+        StringBuilder output= new StringBuilder();
+
+        for (String number : numbers) {
+            output.append(number).append(" ");
+        }
+        System.out.println(output.toString().trim().replaceAll("\\s+"," "));
+    }
+}
