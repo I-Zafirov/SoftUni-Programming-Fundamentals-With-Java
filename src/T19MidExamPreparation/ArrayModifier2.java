4af7bf741e0e50220dd6779c7b280c66d8b762fb
I-Zafirov <ivoomail@gmail.com>
2022-05-09 21:00:35 +0300

    commit

---
+++ src/T19MidExamPreparation/ArrayModifier2.java
@@ -0,0 +1,39 @@
+package T19MidExamPreparation;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class ArrayModifier2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> array = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt).collect(Collectors.toList());
+        String input;
+
+        while (!"end".equals(input = scanner.nextLine())) {
+            String[] command = input.split("\\s+");
+
+            switch (command[0]) {
+                case "swap":
+                    int swapIndex1 = Integer.parseInt(command[1]);
+                    int swapIndex2 = Integer.parseInt(command[2]);
+                    Collections.swap(array, swapIndex1, swapIndex2);
+                    break;
+                case "multiply":
+                    int firstIndex = Integer.parseInt(command[1]);
+                    int secondIndex = Integer.parseInt(command[2]);
+                    array.set(firstIndex, array.get(firstIndex) * array.get(secondIndex));
+                    break;
+                case "decrease":
+                    IntStream.range(0, array.size()).forEach(i -> array.set(i, array.get(i) - 1));
+                    break;
+            }
+        }
+        System.out.print(Arrays.toString(new List[]{array}).replaceAll("[\\[\\]]", ""));
+    }
+}
