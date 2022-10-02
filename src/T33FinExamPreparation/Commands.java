db6ea83bf475b67a247f850511e8db7237e245f4
I-Zafirov <ivoomail@gmail.com>
2022-10-02 09:18:05 +0300

    commit

---
+++ src/T33FinExamPreparation/Commands.java
@@ -0,0 +1,57 @@
+package T33FinExamPreparation;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Commands {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        List<Integer> list  =
+                Arrays.stream(scan.nextLine().split(" "))
+                        .map(Integer::parseInt)
+                        .collect(Collectors.toList());
+        List<Integer> bounds = new ArrayList<>();
+        List<Integer> reverse = reverseList(bounds);
+
+        String[] command = scan.nextLine().split(" ");
+
+
+        while (!command[0].equals("end")){
+
+            switch (command[0]){
+                case "reverse":
+                    int start = Integer.parseInt(command[2]);
+                    int count = Integer.parseInt(command[4]);
+                    for(int i = start; i<list.size(); i++){
+                        if(i==count){
+                            break;
+                        }else{
+                            bounds.add(list.get(i));
+                        }
+                    }
+                    for(int i = 0; i<reverse.size(); i++){
+                        list.add(start, reverse.get(i));
+                    }
+
+                case "sort":
+
+                    break;
+                case "remove":
+
+            }
+            command = scan.nextLine().split(" ");
+
+        }
+    }
+    public static<T> List<T> reverseList(List<T> list)
+    {
+        return list.stream()
+                .collect(Collectors.collectingAndThen(
+                        Collectors.toCollection(ArrayList::new), lst -> {
+                            Collections.reverse(lst);
+                            return lst.stream();
+                        }
+                )).collect(Collectors.toCollection(ArrayList::new));
+    }
+}
