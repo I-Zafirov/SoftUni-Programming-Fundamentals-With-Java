5a195d160f219ff2215d5f34a873ccfb03787797
I-Zafirov <ivoomail@gmail.com>
2022-10-05 10:48:18 +0300

    commit

---
+++ src/T33FinExamPreparation/Concert2.java
@@ -0,0 +1,91 @@
+package T33FinExamPreparation;
+
+import java.util.*;
+
+public class Concert2 {
+    static class Band {
+
+        public int getTime() {
+            return time;
+        }
+
+        public List<String> getMembers() {
+            return members;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public Band(String name) {
+            this.name = name;
+        }
+
+        public void addTime(int time) {
+            this.time += time;
+        }
+
+        public void addMembers(String member) {
+            if (!this.members.contains(member)) {
+                this.members.add(member);
+            }
+        }
+
+        private String name;
+        private int time;
+        private List<String> members = new ArrayList<>();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Map<String, Band> bandsName = new HashMap<>();
+        String input = scanner.nextLine();
+        while (!"start of concert".equals(input)) {
+            String[] tokens = input.split(";+");
+            String command = tokens[0].trim();
+            String bandName = tokens[1].trim();
+            Band band = bandsName.get(bandName);
+            if (band == null) {
+                band = new Band(bandName);
+                bandsName.put(bandName, band);
+            }
+            switch (command) {
+                case "Add":
+                    String[] members = tokens[2].split(", ");
+                    for (String member : members) {
+                        String currMemb = member.trim();
+                        band.addMembers(currMemb);
+                    }
+                    break;
+                case "Play":
+                    int time = Integer.parseInt(tokens[2].trim());
+                    band.addTime(time);
+                    break;
+            }
+            input = scanner.nextLine();
+        }
+        int totalTime = 0;
+        for (Band band : bandsName.values()) {
+            totalTime += band.getTime();
+        }
+        System.out.println("Total time: " + totalTime);
+        bandsName.entrySet().stream().sorted((a, b) -> {
+            int first = a.getValue().getTime();
+            int second = b.getValue().getTime();
+            int comp = second - first;
+            if (comp == 0) {
+                comp = a.getKey().compareTo(b.getKey());
+            }
+            return comp;
+        }).forEach((p) -> {
+            System.out.printf("%s -> %s%n", p.getKey(), p.getValue().getTime());
+        });
+        String bandToPrint = scanner.nextLine();
+        Band band = bandsName.get(bandToPrint);
+        System.out.println(bandToPrint);
+        band.getMembers().forEach((a) -> {
+            System.out.printf("=> %s%n", a);
+        });
+    }
+}
