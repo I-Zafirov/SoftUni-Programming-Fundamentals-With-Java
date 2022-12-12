227020d7cda22b2f1b0898c77434321d6afb73ce
I-Zafirov <ivoomail@gmail.com>
2022-12-12 11:48:54 +0200

    commit

---
+++ src/T33FinExamPreparation/UnlikedMealsStopLikeUnlike.java
@@ -0,0 +1,41 @@
+package T33FinExamPreparation;
+
+import java.util.*;
+
+public class UnlikedMealsStopLikeUnlike {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Map<String, List<String>> guestInfo = new TreeMap<>();
+        String input = scanner.nextLine();
+        int countUnlike = 0;
+        while (!"Stop".equals(input)) {
+            String[] command = input.split("-");
+            switch (command[0]) {
+                case "Like":
+                    guestInfo.putIfAbsent(command[1], new ArrayList<>());
+                    if (!guestInfo.get(command[1]).contains(command[2])) {
+                        guestInfo.get(command[1]).add(command[2]);
+                    }
+                    break;
+                case "Unlike":
+                    if (!guestInfo.containsKey(command[1])) {
+                        System.out.printf("%s is not at the party.%n", command[1]);
+                    } else {
+                        if (!guestInfo.get(command[1]).contains(command[2])) {
+                            System.out.printf("%s doesn't have the %s in his/her collection.%n", command[1], command[2]);
+                        } else {
+                            guestInfo.get(command[1]).remove(command[2]);
+                            System.out.printf("%s doesn't like the %s.%n", command[1], command[2]);
+                            countUnlike++;
+                        }
+                    }
+                    break;
+            }
+        }
+        guestInfo.entrySet().stream().sorted((list1, list2) -> Integer.
+                        compare(list2.getValue().size(), list1.getValue().size()))
+                .forEach(e -> System.out.println(e.getKey() + ": " + String.join(", ", e.getValue())));
+        System.out.printf("Unliked meals: %d", countUnlike);
+    }
+}
